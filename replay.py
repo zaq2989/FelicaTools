1ed005a230b25938ec6315dc9fb8e18fb5365d00
OLIET2357 <oliet2357@gmail.com>
2023-03-03 01:36:41 +0900

    making emulate.py

--- replay.py
+++ replay.py
@@ -1,71 +1,92 @@
 import nfc
 from nfc.clf import LocalTarget, TimeoutError, BrokenLinkError
-import argparse
-import json
+import sys
 fromhex = bytearray.fromhex
 
-parser = argparse.ArgumentParser(
-    description='RePlay FeliCa Exchange', epilog='v1.1')
 
-parser.add_argument('FILE', help='RePlay File')
-parser.add_argument('-d', '--device', help='Device')
-parser.add_argument('-t', '--timeout', help='timeout',
-                    type=float, default=0.005)
+def emulate(FILE, card, DEVICE='usb:054c', TIMEOUT=1.):
+    sensf_res = fromhex('01'+card['idm']+card['pmm']+card['sys'])
 
-args = parser.parse_args()
+    clf = nfc.ContactlessFrontend(DEVICE)
 
-FILE = args.FILE
-DEVICE = 'usb'
-if args.device:
+    print('waiting reader...', file=sys.stderr)
+    target = None
+    while target is None:
+        target = clf.listen(LocalTarget(
+            "212F", sensf_res=sensf_res), timeout=1.)
+
+    tt3_cmd = target.tt3_cmd
+    command = (len(tt3_cmd) + 1).to_bytes(1, "big") + tt3_cmd
+
+    while True:
+        print('<<', command.hex())
+        command_code = command[1]
+        response = None
+        if command_code == 0x00:  # Polling
+            pass  # TODO
+        if command_code == 0x04:  # Request Response
+            idm = command[2:10]
+            response = f'05{idm}00'
+            # TODO
+        if command_code == 0x06:  # Read Without Encryption
+            m = command[11]
+            # TODO
+        if command_code == 0x0C:  # Request System Code
+            pass  # TODO
+
+        if response is None:  # TODO
+            print('', file=sys.stderr)  # TODO
+            continue
+
+        assert isinstance(response, bytearray)
+        print('>>', response.hex())
+        try:
+            command = clf.exchange(response, TIMEOUT)
+        except KeyboardInterrupt:
+            break
+        except TimeoutError:
+            print('TIMEOUT Reader', file=sys.stderr)
+            break
+        except BrokenLinkError:
+            print('Exchange Finished', file=sys.stderr)
+            break
+
+    clf.close()
+
+
+def main(args):
+    FILE = args.FILE
     DEVICE = args.device
-TIMEOUT = args.timeout
+    TIMEOUT = args.timeout
 
-print('TIMEOUT', TIMEOUT, 's')
+    card = dict()
 
-qa = {}
+    for l in open(FILE, 'r'):
+        l = l.replace('\n', '')
+        l = l.replace(' ', '')
+        if l.startswith('#'):
+            continue
+        if '=' in l:
+            i = l.find('=')
+            key, value = l[:i].lower(), l[i+1:].lower()
+            card[key] = value
 
-for l in open(FILE, 'r'):
-    l = l.replace('\n', '')
-    d = l[3:]
-    if l.startswith('<>'):
-        print('<>', d)
-        card = json.loads(d)
-        if card['idm'].lower() == 'random':  # TODO
-            pass
-    if l.startswith('<<'):
-        q = d.lower()
-    if l.startswith('>>'):
-        a = d.lower()
-        qa[q] = a
+    if card['idm'] == 'random':  # TODO
+        import random
+        pass
 
-sensf_res = fromhex('01'+card['idm']+card['pmm']+card['sys'])
+    emulate(**args)
 
-clf = nfc.ContactlessFrontend(DEVICE)
-target = clf.listen(LocalTarget(
-    "212F", sensf_res=sensf_res), timeout=60.)
 
-if target is None:
-    print('No Reader')
-    exit(-1)
+if __name__ == '__main__':
+    import argparse
 
-tt3_cmd = target.tt3_cmd
-rsp = (len(tt3_cmd) + 1).to_bytes(1, "big") + tt3_cmd
+    parser = argparse.ArgumentParser(description='Emulate FeliCa Card')
 
-while True:
-    print('<<', rsp.hex())
-    try:
-        com = fromhex(qa[rsp.hex()])
-        print('>>', com.hex())
-    except KeyError:
-        print('Unknown Command')
-        com = None
-    try:
-        rsp = clf.exchange(com, TIMEOUT)
-    except TimeoutError:
-        print('TIMEOUT Reader')
-        break
-    except BrokenLinkError:
-        print('Exchange Finished')
-        break
+    parser.add_argument('FILE', help='RePlay File')
+    parser.add_argument('-t', '--timeout', type=float)
+    parser.add_argument('--device')
 
-clf.close()
+    args = parser.parse_args()
+
+    main(args)
