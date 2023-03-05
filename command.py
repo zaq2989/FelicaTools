f6d19c33e92eb5ba84464a2b4b56cb763cb14c03
OLIET2357 <oliet2357@gmail.com>
2023-03-05 22:59:41 +0900

    20230305

--- command.py
+++ command.py
@@ -1,88 +1,73 @@
 import nfc
 from nfc.clf import RemoteTarget, TimeoutError
-import argparse
 import sys
 fromhex = bytearray.fromhex
 
 
-def main(args):
-    device = args.device
-    human_mode = not args.machine_mode
-    TIMEOUT = args.timeout
-    system_code = args.system_code
+def command(exchange, system_code):
     assert len(system_code) == 4
-
-    clf = nfc.ContactlessFrontend(device)
-
-    target = RemoteTarget("212F")
-    sensf_req = fromhex(f'00{system_code}0100')
-    target.sensf_req = sensf_req
-    if human_mode:
-        print('<< #', sensf_req.hex())
-    target = clf.sense(target)
-    if target is None:
-        print('No card', file=sys.stderr)
-        exit(1)
-
-    sensf_res = target.sensf_res
-    if human_mode:
-        idm = sensf_res[1:9].hex()
-        print('>> #', sensf_res.hex())
-
+    idm = ''
     while True:
         try:
-            if human_mode:
-                i = input('<< # ')
+            print('<< # ', end='')
+            if idm == '':
+                i = f'00 {system_code} 00 00'
+                print(i)
             else:
-                i = input('<< ')
-            if human_mode:
-                i = i.replace(' ', '')
-                if i == '':
-                    continue
-                i = i.lower().replace('[idm]', idm)
-                i = ('%02x' % (len(i)//2+1)) + i
+                i = input()
+            i = i.replace(' ', '')
+            if i == '':
+                continue
+            i = i.lower().replace('[idm]', idm)
+            i = ('%02x' % (len(i)//2+1)) + i
 
-            r = clf.exchange(fromhex(i), TIMEOUT)
+            r = exchange(fromhex(i))
 
-            if human_mode:
-                if r[1] == 0x01:  # polling
-                    idm = r[2:10].hex()
+            if r[1] == 0x01:  # polling
+                idm = r[2:10].hex()
+                print(f'\t[IDm] set to {idm}', file=sys.stderr)
             h = r.hex()
-            if human_mode:
-                assert r[0] == len(r)
-                h = '# ' + h[2:]
-                h = h.replace(idm, ' [IDm] ')
+            assert r[0] == len(r)
+            h = '# ' + h[2:]
+            h = h.replace(idm, ' [IDm] ')
             print('>>', h)
         except KeyboardInterrupt:
             break
         except EOFError:
             break
         except ValueError as e:
-            if human_mode:
-                print(e, file=sys.stderr)
-            else:
-                break
+            print(e, file=sys.stderr)
         except TimeoutError:
-            if human_mode:
-                print('TIMEOUT', file=sys.stderr)
-            else:
-                continue  # explicitly
+            print('TIMEOUT', file=sys.stderr)
 
-    clf.close()
+
+def main(args):
+    device = args.device
+    timeout = args.timeout
+    system_code = args.system_code
+    try:
+        clf = nfc.ContactlessFrontend(device)
+        target = clf.sense(RemoteTarget("212F"))
+        if target is None:
+            print('No card', file=sys.stderr)
+            exit(1)
+        command(lambda x: clf.exchange(x, timeout), system_code)
+    finally:
+        clf.close()
 
 
 if __name__ == '__main__':
+    import argparse
+
     parser = argparse.ArgumentParser(
         description='Communicate directly with FeliCa')
 
     parser.add_argument('--device', default='usb',
                         help='specify device (DEFAULT:usb)')
-    parser.add_argument('-m', '--machine-mode', action='store_true',
-                        help='turn into machine mode')
     parser.add_argument('-t', '--timeout', type=float, default=1.,
                         help='exchange timeout [s] (DEFAULT:1s)')
     parser.add_argument('-s', '--system-code', default='FFFF',
-                        help='sensing system code (DEFAULT:FFFF)')
+                        help='polling system code (DEFAULT:FFFF)')
 
     args = parser.parse_args()
 
