3437a2094059c434874cd2704842626b21db6075
OLIET2357 <oliet2357@gmail.com>
2023-03-06 22:39:20 +0900

    20230306

--- dump.py
+++ dump.py
@@ -1,50 +1,52 @@
 import nfc
 from nfc.clf import RemoteTarget
+fromhex = bytearray.fromhex
 
 
-def prependLEN(data):
-    return (len(data)+1).to_bytes(1, "big")+data
+def dump(exchange, idm):
+    response_RSC = exchange(fromhex(f'0C {idm.hex()}'))  # Request System Code
+    system_codes = [response_RSC[10:][i*2:(i+1)*2].hex()
+                    for i in range(response_RSC[9])]
 
+    print(f'{system_codes=}')
 
-def dump(clf):
-    target = RemoteTarget("212F")
-    sensf_req = bytearray.fromhex(f'00 FE00 01 00')
-    target.sensf_req = sensf_req
-    target = clf.sense(target)
+    for system_code in system_codes:
+        print(f'{system_code=}')
+        # Polling
+        idm = exchange(fromhex(f'00 {system_code} 00 00'))[1:9].hex()
 
-    assert target is not None
+        service_codes = []
 
-    idm = target.sensf_res[1:9]
+        for i in range(0xFFFF):
+            # Search Service Code
+            command_SSC = fromhex(f'0A {idm}') + i.to_bytes(2, "little")
+            response_SSC = exchange(command_SSC)
 
-    response = clf.exchange(prependLEN(b'\x0C' + idm), 1.)
-    print(f'{response.hex()=}')
+            s = response_SSC[9:].hex()
+            if s == 'ffff':
+                break
 
-    system_codes = response[11:]
+            service_codes.append(s)
 
-    service_codes = []
+        for service_code in service_codes:
+            print(f'{service_code=}')
+            if len(service_code)//2 == 2:
+                # Request Service
+                command_RS = fromhex(f'02 {idm} 01 {service_code}')
+                response_RS = exchange(command_RS).hex()
+                print(f'{response_RS=}')
 
-    for i in range(0xFFFF):
-        command = b'\x0A'+idm+i.to_bytes(2, "little")
-        response = clf.exchange(prependLEN(command), 1.)
-
-        s = response[10:]
-        if s == b'\xFF\xFF':
-            break
-
-        service_codes.append(s)
-
-    for s in service_codes:
-        print(f'{s.hex()=}')
-        if len(s) == 2:
-            command = b'\x02' + idm + b'\x01' + s
-            response = clf.exchange(prependLEN(command), 1.)
-            print(f'{response.hex()=}')
-
-            if response.endswith(b'\x00\x00'):
-
-                command2 = b'\x06' + idm + b'\x01'+s+b'\x01'+b'\x80\x00'
-                response2 = clf.exchange(prependLEN(command2), 1.)
-                print(f'{response2.hex()=}')
+                if response_RS.endswith('0000'):
+                    for block in range(16):  # TODO
+                        # Read Without Encryption
+                        command_RWE = fromhex(f'06 {idm} 01 {service_code} 01') + \
+                            (0x8000+block).to_bytes(2, "big")
+                        response_RWE = exchange(command_RWE)
+                        SF1, SF2 = response_RWE[9], response_RWE[10]
+                        if SF1 != 0x00:
+                            break
+                        data = response_RWE[12:].hex()
+                        print('%04x' % (block), ':', data)
 
 
 def main(args):
@@ -52,9 +54,13 @@ def main(args):
 
     clf = nfc.ContactlessFrontend(device)
 
-    dump(clf)
-
-    clf.close()
+    try:
+        target = clf.sense(RemoteTarget("212F"))
+        assert target is not None, 'No card'
+        idm = target.sensf_res[1:9]
+        dump(lambda c: clf.exchange((len(c)+1).to_bytes() + c, 1.)[1:], idm)
+    finally:
+        clf.close()
 
 
 if __name__ == '__main__':
